32b431e03367c5d0e31d6ef95da7c27f0c07e9eb
akbor shanto <akborshanto1111.com>
2025-04-05 20:00:57 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from 'express'
+
+
+const app = express()
+
+// middleware
+app.use(express.json())
+
+// app.use('/api/user', userRouter)
+// app.use('/api/tour', tourRouter)
+
+// POST: /api/user/create-user
+
+app.get('/', (req: Request, res: Response) => {
+  res.send({
+    status: true,
+    message: 'Server Live ⚡',
+  })
+})
+
+export default app
